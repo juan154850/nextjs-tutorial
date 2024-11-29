82b68d7757b591c9016ca35595417286f1b11b66
juanb <juan@weship.com>
2024-11-29 13:09:07 -0600

    fetch some data from db

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -86,3 +86,10 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
+export interface CardWrapperProps {
+  totalPaidInvoices: number;
+  totalPendingInvoices: number;
+  numberOfInvoices: number;
+  numberOfCustomers: number;
+}
