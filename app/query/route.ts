11ed3116062313876cb85430eb213c63069621a5
juanb <juan@weship.com>
2024-11-29 11:44:08 -0600

    add the database with docker,  change the connection of the database and pupulate the db.

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,34 @@
-// import { db } from "@vercel/postgres";
+import { Client } from "pg";
 
-// const client = await db.connect();
+const client = new Client({
+  user: "userDb",
+  host: "localhost",
+  database: "nextjs-dashboard",
+  password: "passwordDb",
+  port: 5432,
+});
 
-// async function listInvoices() {
-// 	const data = await client.sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+await client.connect();
 
-// 	return data.rows;
-// }
+async function listInvoices() {
+  const data = await client.query(`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `);
+
+  return data.rows;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  // return Response.json({
+  //   message:
+  //     "Uncomment this file and remove this line. You can delete this file when you are finished.",
+  // });
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
